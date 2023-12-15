4b022b54b8ffebe166c202b7344992d1634402fc
Atticus Blanco <atticusbrujx@gmail.com>
2023-12-15 12:58:45 -0800

    main skeleton

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+          private static String[] userPrompt = {
+                  "Would you like to sort a set of",
+                  "Randomized sequential integers",
+                  "Random set of integers",
+                  "Custom set of integers"
+          };
+
+
+          private static void arraySelection() {
+                    int userChoice;
+                    Scanner s = new Scanner(System.in);
+                    System.out.println(userPrompt[0]);
+                    for (int i = 1; i < userPrompt.length; i++) {
+                              System.out.printf("%d. %s\n", i, userPrompt[i]);
+                    }
+                    userChoice = s.nextInt();
+                    switch (userChoice) {
+                              case 1:
+                                        break;
+                              case 2:
+                                        break;
+                              case 3:
+                                        break;
+                              default:
+                                        System.out.println("Invalid choice. Please select 1, 2, or 3.");
+                    }
+
+          }
+          public static void main(String[] arges){
+                    arraySelection();
+          }
+
+
+}
